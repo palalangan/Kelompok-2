225d0f31100314efbadd8dcee481c2af67c48c1d
FriendlyMaabuat <friendlymaabuat25@gmail.com>
2022-03-07 21:38:31 +0800

    First Commit: Nomor 1 dan Nomor 2

--- index.js
+++ index.js
@@ -1,38 +1,45 @@
-const http = require("http");
-const moment = require("moment");
-const members = require("./members.js");
-const users = require("./users.js");
+const { application, request, response } = require("express");
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+let usr = require("./users");
+const { json } = require("express/lib/response");
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  const url = req.url;
-  if (url === "/about") {
-    res.setHeader("Content-Type", "text/json");
-    res.write(
-      JSON.stringify({
-        Status: "success",
-        Message: "response success",
-        Description: "Group Exercise #03",
-        Date: moment().format('2022-02-15T07:51:09+08:00'),
-        Data: members.array()
-      })
-    );
-  } else if (url === "/users") {
-    res.setHeader("Content-Type", "text/json");
-    res.write(
-      JSON.stringify({
-        Data: users.array2()
-      })
-    );
-  } else {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html><body><h2>This is the home page.</h2></body></html>");
-  }
-  res.end();
+//Morgan untuk mencatat log
+app.use(morgan("dev"));
+
+//Penanganan CORS. Tambahkan url: http://127.0.0.1:5500  untuk methods GET, POST, PUT, DELETE
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5500",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log("Time: ", Date.now() + " " + req.ip + " " + req.originalUrl);
+  next();
 });
 
-const hostname = "127.0.0.1"; //localhost
-const port = 3000;
-server.listen(port, hostname, () => {
-  console.log(`Server running at http:/${hostname}:${port}`);
+//1. GET: /users (JADI)
+app.get("/users", (req, res) => res.send(usr));
+// app.get("/", (req, res) => res.json({nama: "pala'langan, radocen chrisnov"}));
+
+//2. GET: /users/:name (JADI TAPI TINGGAL ERROR MESSAGE YANG BELUM)
+app.get("/users/:name", (req, res) => {
+  const result = usr.filter((val) => {
+    return val.name.toLocaleLowerCase() === req.params.name.toLocaleLowerCase();
+  });
+  if (result.length === 0) {
+    return res.json({
+      message: "Data tidak ditemukan",
+    });
+  } else {
+    return res.json(result);
+  }
 });
