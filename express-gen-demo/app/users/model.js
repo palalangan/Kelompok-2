44afb3ba8f0aef310fdf9ad59309cb72753c8407
FriendlyMaabuat <friendlymaabuat25@gmail.com>
2022-05-06 23:46:27 +0800

    Setting up the scheme

--- express-gen-demo/app/users/model.js
+++ express-gen-demo/app/users/model.js
@@ -3,10 +3,16 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   name: { type: String, require: [true, "Nama harus diisi"] },
   age: { type: Number, require: [true, "Age harus diisi"] },
+  team: { type: String, require: [true, "Team harus diisi"] },
+  game: {
+    type: String,
+    enum: ["Dota2", "VALORANT", "Apex Legends", "PUBG", "CSGO"],
+    default: "Dota2",
+  },
   status: {
     type: String,
-    enum: ["active", "non active"],
-    default: "non active",
+    enum: ["Captain", "Member", "Coach"],
+    default: "Captain",
   },
 });
 
